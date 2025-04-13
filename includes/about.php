db9ab6d80fa7c89c9ddf392d1fecad878cd7fd1d
Sulaimon Yusuf <codewithemperor@gmail.com>
2025-04-14 00:02:22 +0100

    modified:   about.php 	modified:   images/about-two-img-3.jpg 	new file:   images/about/about-img (1).jpg 	new file:   images/about/about-img (10).jpg 	new file:   images/about/about-img (2).jpg 	new file:   images/about/about-img (3).jpg 	new file:   images/about/about-img (4).jpg 	new file:   images/about/about-img (5).jpg 	new file:   images/about/about-img (6).jpg 	new file:   images/about/about-img (7).jpg 	new file:   images/about/about-img (8).jpg 	new file:   images/about/about-img (9).jpg 	new file:   includes/about.php 	modified:   index.php

---
+++ includes/about.php
@@ -0,0 +1,47 @@
+<section class="company section" id="about">
+    <div class="container">
+        <div class="row align-items-center">
+
+            <!-- Section Title -->
+            <div class="col-12 text-center">
+                <h2 class="title mb-5" data-aos="fade-down" data-aos-delay="100">Our History</h2>
+            </div>
+
+            <!-- Content Section -->
+            <div class="col-lg-6 px-3">
+                <div class="content" data-aos="fade-right" data-aos-delay="200">
+
+                    <p class="mb-3" data-aos="fade-up" data-aos-delay="400">
+                        With over 15 years of experience, Kunbis Unique Ventures has been delivering outstanding
+                        services in Travel and Tours, Estate Management, and Automobile. Our dedication goes beyond just meeting industry standards; we aim to elevate your business and lifestyle through tailored solutions.
+                    </p>
+
+                    <ul class="list-unstyled ms-2">
+                        <li data-aos="fade-up" data-aos-delay="500">Registered with the Nigerian government and committed to quality service.</li>
+                        <li data-aos="fade-up" data-aos-delay="600">A long-standing reputation built over 15+ years in business.</li>
+                        <li data-aos="fade-up" data-aos-delay="700">Professional expertise across our subdivisions: Travel & Tours, Estate Management, and Automobile.</li>
+                        <li data-aos="fade-up" data-aos-delay="800">We offer free initial consultation and personalized support to meet your unique needs.</li>
+                        <li data-aos="fade-up" data-aos-delay="900">Our services are trusted by some of the most successful businesses and clients.</li>
+                        <li data-aos="fade-up" data-aos-delay="1000">A dedicated team of professionals committed to driving growth and delivering results.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Image Section -->
+            <div class="col-lg-6">
+                <div class="img d-flex position-relative">
+                    <div class="imgone" data-aos="fade-down" data-aos-delay="600">
+                        <div class="simpleParallax" style="overflow: hidden;">
+                            <img class="thumparallax-down" src="images/about/about-img (<?php echo rand(1, 10); ?>).jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="imgtwo" data-aos="fade-up" data-aos-delay="800">
+                        <div class="simpleParallax" style="overflow: hidden;">
+                            <img class="thumparallax-down" src="images/about/about-img (<?php echo rand(1, 10); ?>).jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
